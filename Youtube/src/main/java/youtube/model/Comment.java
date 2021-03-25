2c245791a7e4ec61a16193ba842364c6ae6b92b4
Hristiyan <haripetkov991226@gmail.com>
2021-03-25 12:36:49 +0200

    Linked classes with database tables.

--- Youtube/src/main/java/youtube/model/Comment.java
+++ Youtube/src/main/java/youtube/model/Comment.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
+@Table(name = "comments")
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- Youtube/src/main/java/youtube/model/Playlist.java
+++ Youtube/src/main/java/youtube/model/Playlist.java
@@ -4,16 +4,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
+@Table(name = "playlists")
 public class Playlist {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

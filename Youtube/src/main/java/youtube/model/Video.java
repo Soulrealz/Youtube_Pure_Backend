01e5d5ada54f6b307b00349883fbeefbcc7f4266
Boris Hristov <bdhristovv@gmail.com>
2021-03-25 12:24:02 +0200

    updated database

--- Youtube/src/main/java/youtube/model/Video.java
+++ Youtube/src/main/java/youtube/model/Video.java
@@ -5,16 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
+@Table(name = "videos")
 public class Video {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

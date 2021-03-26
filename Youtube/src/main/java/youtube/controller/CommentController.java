f6beb53694205a5fa6c7049db9ef43836a6aa5fb
Boris Hristov <bdhristovv@gmail.com>
2021-03-26 17:09:55 +0200

    added controller for comments with a supposedly working make comment on X video method, not fully tested

--- Youtube/src/main/java/youtube/controller/CommentController.java
+++ Youtube/src/main/java/youtube/controller/CommentController.java
@@ -1,4 +1,24 @@
 package youtube.controller;
 
-public class CommentController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import youtube.model.dto.CommentDTO;
+import youtube.model.pojo.User;
+import youtube.model.services.CommentService;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class CommentController extends AbstractController {
+    @Autowired
+    private CommentService commentService;
+    @Autowired
+    private SessionManager sessionManager;
+
+    // Making comments on X video
+    @PutMapping("/videos/{id}")
+    public CommentDTO makeComment(@RequestBody String text, @PathVariable String id, HttpSession ses) {
+        User u = sessionManager.getLoggedUser(ses);
+        return commentService.makeComment(id, text, u);
+    }
 }

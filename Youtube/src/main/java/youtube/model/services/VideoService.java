db73f9869e93dd0617ab5ba71522bceff770bcbd
Boris Hristov <bdhristovv@gmail.com>
2021-03-26 15:34:42 +0200

    added video service with tested method for getting all videos with a certain title

--- Youtube/src/main/java/youtube/model/services/VideoService.java
+++ Youtube/src/main/java/youtube/model/services/VideoService.java
@@ -1,4 +1,40 @@
 package youtube.model.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import youtube.exceptions.NotFoundException;
+import youtube.model.dto.VideoWithoutIDDTO;
+import youtube.model.pojo.Video;
+import youtube.model.repository.VideoRepository;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class VideoService {
+    @Autowired
+    private VideoRepository videoRepository;
+
+    /*
+    public VideoWithoutIDDTO getVideoByID(int id) {
+        Optional<Video> video = videoRepository.findById(id);
+        if (video.isEmpty()) {
+            throw new NotFoundException("No such video exists");
+        }
+        return new VideoWithoutIDDTO(video.get());
+    }
+    */
+
+
+    public List<VideoWithoutIDDTO> getAllVideos(String title) {
+        // Building a list of Videos to return Videos without ID
+        List<VideoWithoutIDDTO> ls = new LinkedList<>();
+        var v = videoRepository.findAllByTitle(title);
+        for (Video video : v) {
+            ls.add(new VideoWithoutIDDTO(video));
+        }
+
+        return ls;
+    }
 }

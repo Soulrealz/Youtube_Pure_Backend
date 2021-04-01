b5f2cf7d24916569520f53b0ca8297268a55bd28
Boris Hristov <bdhristovv@gmail.com>
2021-04-01 03:23:07 +0300

    swagger added

--- Youtube/src/main/java/youtube/YoutubeApplication.java
+++ Youtube/src/main/java/youtube/YoutubeApplication.java
@@ -2,8 +2,19 @@ package youtube;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @SpringBootApplication
+//@EnableSwagger2
 public class YoutubeApplication {
 
     public static void main(String[] args) {
@@ -11,4 +22,25 @@ public class YoutubeApplication {
 
     }
 
+    @Bean
+    public Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "Youtube API",
+                "Every endpoint and request sample",
+                "1.0",
+                null,
+                null,
+                null,
+                null,
+                Collections.emptyList()
+        );
+    }
 }

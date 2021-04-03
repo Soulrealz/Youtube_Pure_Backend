45819433a5af5ba23fcd7823ceedc6468c544373
Boris Hristov <bdhristovv@gmail.com>
2021-04-03 23:41:50 +0300

    added custom Pair DS for video and like count

---
+++ Youtube/src/main/java/youtube/model/validations/PairVideoInt.java
@@ -0,0 +1,16 @@
+package youtube.model.validations;
+
+import youtube.model.pojo.Video;
+
+public class PairVideoInt {
+    Video video;
+    int likes;
+
+    public PairVideoInt(Video video, int likes) {
+        this.video = video;
+        this.likes = likes;
+    }
+
+    public Video getVideo() { return video; }
+    public int getLikes() { return likes; }
+}

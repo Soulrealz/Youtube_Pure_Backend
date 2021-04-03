6e1a79b2791191fe9040a2c1901141ace9e31753
Boris Hristov <bdhristovv@gmail.com>
2021-04-03 16:43:43 +0300

    removed unnecessary testing stuff from emailservice

--- Youtube/src/main/java/youtube/model/services/EmailService.java
+++ Youtube/src/main/java/youtube/model/services/EmailService.java
@@ -12,13 +12,8 @@ public class EmailService {
     private JavaMailSender javaMailSender;
     @Value("${spring.mail.username}")
     private String email;
-    @Value("${spring.mail.password}")
-    private String pass;
 
     public void sendEmail(String to, String body, String topic) {
-        System.out.println("Sending mail...");
-        System.out.println("From " + email);
-        System.out.println("Password: " + pass);
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
 
         simpleMailMessage.setFrom(email);
@@ -26,6 +21,5 @@ public class EmailService {
         simpleMailMessage.setSubject(topic);
         simpleMailMessage.setText(body);
         javaMailSender.send(simpleMailMessage);
-        System.out.println("Mail sent!");
     }
 }

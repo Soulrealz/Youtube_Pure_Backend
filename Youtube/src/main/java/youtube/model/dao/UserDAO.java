4f2bf0898e0dc97ff5323bdee71893a7c73a6a9d
Boris Hristov <bdhristovv@gmail.com>
2021-04-05 20:43:08 +0300

    added actual DAO for searchByName, previous commit of this was wrong dao

---
+++ Youtube/src/main/java/youtube/model/dao/UserDAO.java
@@ -0,0 +1,48 @@
+package youtube.model.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import youtube.model.pojo.User;
+import youtube.model.utils.Log4JLogger;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserDAO {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private static final String selectAllWhereUsernameLike =
+            "SELECT * FROM users WHERE username LIKE ?;";
+
+    public List<User> searchByName(String likeParam) {
+        List<User> users = new ArrayList<>();
+        String param = "%" + likeParam +"%";
+
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(selectAllWhereUsernameLike);
+            ps.setString(1, param);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                User user = new User(rs.getInt("id"),
+                        rs.getString("username"),
+                        rs.getBoolean("verified"));
+
+                if (user.getVerified()) {
+                    users.add(user);
+                }
+            }
+            return users;
+        } catch (SQLException throwables) {
+            Log4JLogger.getLogger().error("Could not execute SQL query.\n", throwables);
+            throw new youtube.exceptions.SQLException("Unavailable resource");
+        }
+    }
+}

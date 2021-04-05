eff1fc10faf4f8caa203f07dcde67af3c375b66b
Boris Hristov <bdhristovv@gmail.com>
2021-04-05 20:36:24 +0300

    renamed exceptions

--- Youtube/src/main/java/youtube/exceptions/CustomFileNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package youtube.exceptions;
-
-public class CustomFileNotFoundException extends RuntimeException {
-    public CustomFileNotFoundException(String msg) {
-        super(msg);
-    }
-}
---
+++ Youtube/src/main/java/youtube/exceptions/FileNotFoundException.java
@@ -0,0 +1,7 @@
+package youtube.exceptions;
+
+public class FileNotFoundException extends RuntimeException {
+    public FileNotFoundException(String msg) {
+        super(msg);
+    }
+}

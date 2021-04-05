33c6d50f6eb87b22dab2c479cc7bb804bd752d67
Boris Hristov <bdhristovv@gmail.com>
2021-04-05 03:26:20 +0300

    Logging class added with log4j

---
+++ Youtube/src/main/java/youtube/model/utils/Log4JLogger.java
@@ -0,0 +1,54 @@
+package youtube.model.utils;
+
+import org.apache.log4j.*;
+import org.springframework.beans.factory.annotation.Value;
+import youtube.exceptions.CustomFileNotFoundException;
+import youtube.exceptions.CustomIOException;
+
+import java.io.*;
+
+public class Log4JLogger {
+    private static Logger logger;
+
+    private Log4JLogger() {}
+
+    public static Logger getLogger() {
+        if (logger == null) {
+            Appender appender = new Appender();
+            logger = Logger.getLogger(Log4JLogger.class);
+        }
+        return logger;
+    }
+
+    private static class Appender {
+        // date, priority, category, class name, message, newline
+        // eg : 2021-04-05 02:28:14,051 [ERROR|youtube.model.utils.Log4JLogger|YoutubeApplication] msg
+        private final String PATTERN = "%d [%p|%c|%C{1}] %m%n";
+        private FileAppender fileAppender;
+
+        public Appender() {
+            String path = getLogsPath();
+            fileAppender = new FileAppender();
+            Logger.getRootLogger().addAppender(fileAppender);
+
+            fileAppender.setFile(path);
+            fileAppender.setThreshold(Level.ALL);
+            fileAppender.setLayout(new PatternLayout(PATTERN));
+            fileAppender.activateOptions();
+        }
+
+        private String getLogsPath() {
+            try {
+                BufferedReader file = null;
+                file = new BufferedReader(new FileReader("pathToLogs.txt"));
+                return file.readLine();
+            } catch (FileNotFoundException e) {
+                Log4JLogger.getLogger().error("Logs.txt file not found.\n", e);
+                throw new CustomFileNotFoundException("File not found");
+            } catch (IOException e) {
+                Log4JLogger.getLogger().error("Critical IO exception.\n", e);
+                throw new CustomIOException("Bad Input/Output");
+            }
+        }
+    }
+}
